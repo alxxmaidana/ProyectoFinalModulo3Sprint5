8163b5980f86690fa297dabf4d3fe62e1bebf2a7
Alexander Maidana <alexanderemanuelmaidana@gmail.com>
2026-05-17 21:34:44 -0300

    Cree middleware para parsear campos de strings a arrays, y cada valor del campo fronteras a mayúsculas

--- src/middlewares/parseFields.js
+++ src/middlewares/parseFields.js
@@ -1,13 +1,21 @@
-const convertirStringsAArray = (req, _res, next) => {
+import { body } from "express-validator";
+
+const parsearCampos = (req, _res, next) => {
+    // Convertir campos strings a array 
     const campos = ["capital", "fronteras", "zonasHorarias"];
     campos.forEach((campo) => {
-        // Verificar si campo está en el body y si es un string
+        // Verificar si el campo está en el body y si es un string
         if (req.body[campo] && typeof req.body[campo] === "string") {
             // Separa en comas, elimina espacios y filtra valores falsy (null, "", undefined)
-            req.body[campo].split(",").map((item) => item.trim().filter(Boolean))
+            req.body[campo] = req.body[campo].split(",").map((item) => item.trim()).filter(Boolean);
+            // Si el campo es fronteras convertir los valores a mayúsculas
+            if (req.body[campo] === req.body["fronteras"]) {
+                req.body[campo] = req.body[campo].map((item) => item.toUpperCase())
+            }
+            console.log(req.body[campo])
         }
     });
-    next(); // Continuar el flujo de la petición
+    next()
 }
 
-export default convertirStringsAArray;
+export default parsearCampos;
