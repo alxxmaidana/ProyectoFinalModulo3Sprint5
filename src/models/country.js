04d63696ca6037409a6bfd58c6f9edd578fe11d0
Alexander Maidana <alexanderemanuelmaidana@gmail.com>
2026-05-17 21:26:04 -0300

    Corregir validaciones de el esquema de Países

--- src/models/country.js
+++ src/models/country.js
@@ -7,22 +7,24 @@ dotenv.config();
 /////////////////////////
 const paisesSchema = new mongoose.Schema({
     nombre: {
-        comun: { type: String, trim: true, required: true, minLength: 3, maxLength: 90, },
-        oficial: { type: String, trim: true, required: true, minLength: 3, maxLength: 90, }
+        comun: { type: String, trim: true, required: true, minlength: 3, maxlength: 90, },
+        oficial: { type: String, trim: true, required: true, minlength: 3, maxlength: 90, }
     },
 
     bandera: { type: String, trim: true, }, // URL PNG -> Consumir una API de banderas de países
 
-    capital: [{ type: String, trim: true, required: true }], // En la API de Rest Countries capital suele venir cómo un array.
+    capital: { 
+        type: [{ type: String, trim: true }],
+        validate: {
+            validator: (valor) => valor.length >= 1,
+            message: "El campo valor es obligatório" 
+        }
+    }, // En la API de Rest Countries capital suele venir cómo un array.
 
     subregion: {
         type: String,
         trim: true,
         enum: [ // Verificar que sea algunos de estos valores
-            "Sudamérica",
-            "América Central",
-            "El Caribe (América Insular)",
-            "Norteamérica",
             "Sin Subregión",
             "South America",
             "Central America",
@@ -32,37 +34,35 @@ const paisesSchema = new mongoose.Schema({
         required: true,
     },
 
-    fronteras: [{ type: String, trim: true, minLength: 3, maxLength: 3, uppercase: true, }],
+    fronteras: [{ type: String, trim: true, minlength: 3, maxlength: 3 }],
 
     area: { type: Number, required: true, min: 0, },
 
     poblacion: { type: Number, required: true, min: 0, },
     
-    zonasHorarias: [{
-        type: String,
-        enum: ["UTC-03:00", "UTC-04:00", "UTC-05:00", "UTC-06:00", "UTC-07:00", "UTC-08:00", "UTC-09:00"],
-        trim: true,
-        required: true,
-    }],
+    zonasHorarias: {
+       type: [{
+            type: String,
+            enum: ["UTC-03:00", "UTC-04:00", "UTC-05:00", "UTC-06:00", "UTC-07:00", "UTC-08:00", "UTC-09:00"],
+            trim: true,
+        }],
+        validate: {                             // ← igual que capital
+            validator: (valor) => valor.length >= 1,
+            message: "Elija al menos una zona horaria"
+        }
+    },
 
     moneda: {
-        simbolo: { type: String, trim: true, required: true, maxLength: 5 },
-        nombre: { type: String, trim: true, required: true, minLength: 3, maxLength: 40, }
+        simbolo: { type: String, trim: true, required: true, maxlength: 5 },
+        nombre: { type: String, trim: true, required: true, minlength: 3, maxlength: 40, }
     },
 
     indiceGini: { // Algunos paises no tienen dato
         valor: { type: Number, min: 0, max: 100 },
-        anio: { type: Number,
-            validate: { // Validar que se un entero
-                validator: function(value) {
-                    if (Number.isInteger(value)) return;
-                },
-                message: "El campo anio debe ser un número entero"
-            }
-        }
+        anio: { type: Number, min: 1912, max: new Date().getFullYear() } // Recortar el rango del año 1912 - 2026;
     },
 
-    tipoDocumento: { type: String, default: "pais", trim: true, requiered: true },
+    tipoDocumento: { type: String, default: "pais", trim: true, required: true },
 
     creador: { type: String, default: process.env.CREATOR, trim: true, required: true }
 },
