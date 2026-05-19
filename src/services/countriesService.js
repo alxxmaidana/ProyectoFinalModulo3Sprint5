a42a399fde9cf25b31ee27072f6054d07cb2cd86
Alexander Maidana <alexanderemanuelmaidana@gmail.com>
2026-05-18 21:52:08 -0300

    Agregar funcion para recopilar las subregiones, urls de banderas y zonasHorarias de los países, función instancia y llama al método para guardar el documento con los datos para formularios, función para obtener el documento con esos datos. Corregir función de cargar paises hispanos, intanciado cada documento antes de llamar al método

--- src/services/countriesService.js
+++ src/services/countriesService.js
@@ -1,14 +1,17 @@
+import { syncBuiltinESMExports } from "node:module";
 import CountriesRepository from "../repositories/CountriesRepository.js";
 import dotenv from "dotenv";
+import Paises from "../models/country.js";
+import PaisesData from "../models/countriesData.js";
 dotenv.config();
 
 // Filtrar paises con idioma español
 export function filtrarPaisesHispanohablantes(paises) {
-    return paises.filter(pais => pais.languages?.spa);
+    return paises.filter(pais => pais.languages.spa);
 }
 
 //////////////////////////////////////////////
-// Convertir los paises al esquema definido
+// Obtener los datos y formatearlos al esquema
 //////////////////////////////////////////////
 export function mapearPaisesHispanos(paisesHispanos) {
     return paisesHispanos.map(pais => {
@@ -41,33 +44,78 @@ export function mapearPaisesHispanos(paisesHispanos) {
     });
 }
 
-// Cargar todos los paíse hispanos de américa de la API
-export async function cargarPaisesHispanohablantes(paises) {
+// Instanciar y llamar al método para guardar los paises
+export async function cargarPaisesHispanohablantes(paisesHispanos) {
+    const paises = paisesHispanos.map(pais => new Paises(pais));
     return await CountriesRepository.cargarPaisesHispanos(paises);
 }
 
-// Llamar al repositorio para agregar el país
+// Llamár el método para agregar el país
 export async function agregarPais(paisAgregar) {
     return await CountriesRepository.agregar(paisAgregar);
 }
 
-// Obtener todos los países de la colección
+// Crear filtro y llamar al método para obtener todos los países de la colección
 export async function obtenerTodosLosPaises() {
     const condicion = { $and: [ {tipoDocumento: "pais"}, {creador: process.env.CREATOR} ]};
     return await CountriesRepository.obtenerTodos(condicion);
 }
 
-// Buscar un país por id
+// Llamar al método para buscar un país por id
 export async function buscarPaisPorId(id) {
     return await CountriesRepository.buscarPorId(id);
 }
 
-// Editar un país por id
+// Llamar método para actualizar el documento
 export async function editarPais(id, paisActualizado) {
     return await CountriesRepository.editar(id, paisActualizado);
 }
 
-// Eliminar un país por id
+// Llamar método para eliminar el recurso
 export async function eliminarPais(id) {
     return await CountriesRepository.eliminar(id);
 }
+
+// Función para obtener las URLs de banderas, zonasHorarias y subregiones de los países de América
+export function obtenerDatosPaises(paises) {
+    let banderasURL = [];
+    let zonasHorarias = [];
+    let subregiones = [];
+
+    // Obtener URLs del png de las banderas
+    paises.forEach((pais) => {
+        banderasURL.push(pais.flags.png);
+        // Obtener zonas horarias
+        const zonasHorariasPais = pais.timezones;
+        for (let i = 0; i < zonasHorariasPais.length; i++) {
+            // Si alguna zona horaria del país no esta en zonasHorarias la incluye
+            if (!zonasHorarias.includes(zonasHorariasPais[i])){ 
+                zonasHorarias.push(zonasHorariasPais[i])
+            }
+        } 
+        // Obtener subregiones
+        const subregionPais = pais.subregion;
+        // Si subregion no están en el array la agrega
+        if (!subregiones.includes(subregionPais)) {
+            subregiones.push(subregionPais)
+        }
+    });
+    // Agregar un valor más al array de subregiones
+    subregiones.push("Sin Subregión");
+
+    // Retorarlo cómo un objeto
+    return { banderasURL, zonasHorarias, subregiones };
+}
+
+// Instanciar y llamar método para guardar el documento con los datos para formulario
+export async function guardarDataPaises(paisesData) {
+    const data = new PaisesData(paisesData)
+    return await CountriesRepository.guardarData(data);
+}
+
+// Obtener data
+export async function obtenerDataPaises() {
+    const condicion = { $and: [ { tipoDocumento: "data", creador: process.env.CREATOR } ] };
+    return await CountriesRepository.obtenerData(condicion);
+}
+
