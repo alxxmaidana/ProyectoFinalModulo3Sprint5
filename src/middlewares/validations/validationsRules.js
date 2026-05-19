281051268cd7e8692c20ad2bdb3663c3a453f6e6
Alexander Maidana <alexanderemanuelmaidana@gmail.com>
2026-05-18 21:21:40 -0300

    Actualizar y comentar reglas de validación para paises

--- src/middlewares/validations/validationsRules.js
+++ src/middlewares/validations/validationsRules.js
@@ -1,18 +1,26 @@
 import { body } from "express-validator";
 
 export const validacionesPaises = [
-    // Validar nombre.comun
+    ////////////////////////
+    // Validaciones nombres
+    ///////////////////////
+
+    // Nombre común
     body("nombre.comun")
-        .trim() // Eliminar los espacios el blanco al inicio y final
-        .notEmpty().withMessage("El nombre común es obligatorio") // Validar que campo no esté vació
+        // Eliminar los espacios el blanco al inicio y final
+        .trim()
+        // Validar que campo no esté vació
+        .notEmpty().withMessage("El nombre común del país es obligatorio")
+        // bail() -> Detiene la ejecución de la cadena de validaciones si la validación falló
         .bail()
         .isString().withMessage("El nombre común debe ser un texto")
-        .bail() // bail() -> Detiene la ejecución de la cadena de validaciones si la validación falló
-        .isLength({ min: 3 }).withMessage("El nombre común debe tener un mínimo de 3 caractéres") // Que tenga un mínimo de 3 caractéres
+        .bail() 
+        // Que tenga un mínimo de 3 caractéres
+        .isLength({ min: 3 }).withMessage("El nombre común debe tener un mínimo de 3 caractéres")
         .bail()
-        .isLength({ max: 90 }).withMessage("El nombre común no puede superar los 60 caractéres"), // Que tenga un máximo de 90 caractéres
-
-    // Validar nombre.oficial
+        // Que tenga un máximo de 90 caractéres
+        .isLength({ max: 90 }).withMessage("El nombre común no puede superar los 60 caractéres"),
+    // Nombre oficial
     body("nombre.oficial")
         .trim()
         .notEmpty().withMessage("El nombre oficial es obligatorio")
@@ -27,23 +35,27 @@ export const validacionesPaises = [
     body("bandera")
         .optional({ values: "falsy" })  // ← ignora "", null, undefined, 0
         .trim()
+        // Que tenga formato de URL
         .isURL().withMessage("El campo bandera debe ser una URL válida"),
 
-    // Validar el array capital
+    ////////////
+    // Capital 
+    ////////////
+    body("capital.*") // Eliminar espacios en blanco de cada capital
+        .trim(),
     body("capital")
-        // Validar que no sea una array vacio
-        .isArray({ min: 1 }).withMessage("La capital es obligatoria")
-        // Validar que cada capital sea un texto (string)
+        // Que el array capital no esté vacío
+        .isArray({ min: 1 }).withMessage("La capital es obligatória")
+        .bail()
+        // Cáda capital sea un texto  
         .custom((capitales) => {
+            // Sin almenos uno no cumple con la validación muestra el mensaje de corrección una sola vez
             if (capitales.some((capital) => typeof capital !== "string")) {
                 throw new Error("Cada capital debe ser un texto")
             }
             return true;
-        }),
-    body("capital.*")
-        .trim(),
-    body("capital")
-        // Validar que cada capital supere los 3 caracteres
+        })
+        // Cada capitlal tenga alménos 3 caracteres
         .custom((capitales) => {
             if (capitales.some((capital) => capital.length < 3)) {
                 throw new Error("Cada capital debe tener almenos 3 caractéres")
@@ -51,33 +63,29 @@ export const validacionesPaises = [
             return true;
         })
         .bail()
-        // Validar que cada capital no supero los 90 caracteres
+        // No superen los 90 caracteres
         .custom((capitales) => {
             if (capitales.some((capital) => capital.length > 90)) {
                 throw new Error("Cada capital no puede superara los 90 caractéres")
             }
             return true;
         }),
+    // Validando de forma con some() evita enviar mensajes de corrección repetidos para cada elemento que no las cumpla
 
-    // Validar subregión
+
+    /////////////////////
+    // Subregión
+    //////////////////
     body("subregion")
         .trim()
-        .notEmpty().withMessage("La subregión es obligatoria")
-        .bail()
-        .isIn([ // Validar que el valor del campo se alguno de los siguientes
-            "Sudamérica",
-            "América Central",
-            "El Caribe (América Insular)",
-            "Norteamérica",
-            "Sin Subregión",
-            "South America",
-            "Central America",
-            "North America",
-            "Caribbean"
-        ]).withMessage("Subregión inválida"),
+        // Subregión obligatória
+        .notEmpty().withMessage("La subregión es obligatoria"),
 
-    // Validar las fronteras 
-    body("fronteras.*") // Eliminar espacios en blanco de cada país de frontereas
+    ////////////////////////
+    // Fronteras
+    ////////////////////////
+    body("fronteras.*")
+    // Eliminar espacios en blanco de cada país de frontereas
         .trim(),
     // Validar que todos los países sean solo letras
     body("fronteras")
@@ -88,7 +96,7 @@ export const validacionesPaises = [
             }
             return true;
         }),
-    // Validar longitud exácta de cada país
+    // Validar longitud exácta de cada país (3 caracteres)
     body("fronteras")
         .optional()
         .custom(fronteras => {
@@ -97,62 +105,88 @@ export const validacionesPaises = [
             }
             return true;
         }),
+    // Validar que esté en mayúsculas
+    body("fronteras")
+        .optional()
+        .custom(fronteras => {
+            if (fronteras.some(frontera => frontera !== frontera.toUpperCase())) {
+                throw new Error("El código de cada frontera deber ser estar en mayúsculas");
+            }
+            return true;
+        }),
 
-    // Validar área del país
+    //////////////////////
+    // Área
+    /////////////////////
     body("area")
         .trim()
         .notEmpty().withMessage("El área del país es obligatório")
         .bail()
+        // Si Es un número
         .isNumeric().withMessage("El área del país debe ser un número")
         .bail()
-        .custom(value => { // Validar que no sea negativo
+        //  Que no sea negativo
+        .custom(value => {
             if (Number(value) < 0) {
                 throw new Error("El área del país no puede ser negativo")
             }
             return true;
         }),
 
-    // Validar poblacion
+    ////////////////////
+    // Población
+    ////////////////////
     body("poblacion")
         .trim()
         .notEmpty().withMessage("La cantidad de habitantes del país es obligatória")
         .bail()
-        .isNumeric().withMessage("La cantidad de habitantes debe ser un número")
+        .isNumeric().withMessage("La cantidad de habitantes debe ser un número entero")
         .bail()
-        .custom(value => { // Validar que no sea negativo
+        // Que población sea entero
+        .isInt().withMessage("La cantidad de habitantes debe ser un número entero")
+        .bail()
+        // Que no sea negativo
+        .custom(value => {
             if (Number(value) < 0) {
                 throw new Error("La cantidad de habitantes del país no puede ser negativa")
             }
             return true;
         }),
 
-    // Validar zonas horárias
+    ///////////////////////
+    // Zonas horárias
+    ///////////////////////   
+    body("zonasHorarias.*")
+        .trim(),
     body("zonasHorarias")
         // Verificar que tenga almenos una zona horaria
         .isArray({ min: 1 }).withMessage("Eliga almenos una zona horária"),
-    // Validar cada zona horaria 
-    body("zonasHorarias.*")
-        .trim()
-        .isIn(["UTC-03:00", "UTC-04:00", "UTC-05:00", "UTC-06:00", "UTC-07:00", "UTC-08:00", "UTC-09:00"])
-        .withMessage("Zona horária invalida"),
-
-    // Validar monedas 
+    
+    //////////////////////
+    // Monedas
+    //////////////////////
     body("moneda.simbolo")
         .trim()
         .notEmpty().withMessage("El símbolo de la moneda es requerido")
         .bail()
+        // Que sea un string
         .isString().withMessage("El símbolo de la moneda debe ser un texto")
         .bail()
+        // Que tenga cómo máximo 5 carácteres
         .isLength({ max: 5 }).withMessage("El símbolo no puede superar los 5 caracteres"),
     body("moneda.nombre")
         .trim()
         .notEmpty().withMessage("El nombre de la moneda es requerido")
         .bail()
+        // Que se texto
         .isString().withMessage("El nombre de la moneda deber ser un texto")
         .bail()
+        // Que tenga cómo minimo 3 caracteres y máximo 40
         .isLength({ min: 3, max: 40 }).withMessage("El nombre debe tener entre 3 y 40 caracteres"),
 
-    // Validar indice gini 
+    //////////////////////////////
+    // Indice de Gini
+    ////////////////////////////
     body("indiceGini")
         .optional()
         .custom((indiceGini) => {
@@ -160,12 +194,14 @@ export const validacionesPaises = [
             if (!indiceGini) return true;
 
             const { valor, anio } = indiceGini;
-            // Si se envía uno solo de los campos
+            
+            // Valida que se halla enviado ambos campos
             if ((valor !== undefined && anio === undefined) || (valor === undefined && anio !== undefined)) {
-                throw new Error("Si se envía indiceGini, debe incluir el valor y año");
+                throw new Error("Si se envía indiceGini, debe incluir el valor y año de medición");
             }
             return true;
         }),
+    // Indice Gini valor
     body("indiceGini.valor")
         .optional()
         .trim()
@@ -179,18 +215,21 @@ export const validacionesPaises = [
             }
             return true;
         }),
-    // Validar que anio se un año válido
+    // Año de medición
     body("indiceGini.anio")
         .optional()
         .trim()
         .isNumeric("El año deber ser un número entero")
+        .bail()
+        // Validar que sea entero
+        .isInt().withMessage(`El año de medición del indice de Gini deber ser un valor entero entre 1912 y ${new Date().getFullYear}`)
         .bail()
         // Validar que el año de medición del indice de gini este entre 1900 y 2026
         .custom((anio) => {
             anio = Number(anio);
             const anioActual = new Date().getFullYear(); // new Date().getFullYear() -> obtiene el año acual
             if (anio < 1912 || anio > anioActual) {
-                throw new Error("El año de medición del cóeficiente de Gini debe ser desde 1912 a 2026");
+                throw new Error(`El año de medición del cóeficiente de Gini debe ser desde 1912 a ${new Date().getFullYear}`);
             }
             return true;
         })
