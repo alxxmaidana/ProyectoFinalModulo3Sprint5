c3c47982bf57f3f7ac11d49333852f6f143bc65b
Alexander Maidana <alexanderemanuelmaidana@gmail.com>
2026-05-18 21:25:35 -0300

    Remover parseo de los las fronteras a Mayúsculas

--- src/middlewares/parseFields.js
+++ src/middlewares/parseFields.js
@@ -1,18 +1,13 @@
 import { body } from "express-validator";
 
+// Parsear strings a array
 const parsearCampos = (req, _res, next) => {
-    // Convertir campos strings a array 
     const campos = ["capital", "fronteras", "zonasHorarias"];
     campos.forEach((campo) => {
         // Verificar si el campo está en el body y si es un string
         if (req.body[campo] && typeof req.body[campo] === "string") {
             // Separa en comas, elimina espacios y filtra valores falsy (null, "", undefined)
             req.body[campo] = req.body[campo].split(",").map((item) => item.trim()).filter(Boolean);
-            // Si el campo es fronteras convertir los valores a mayúsculas
-            if (req.body[campo] === req.body["fronteras"]) {
-                req.body[campo] = req.body[campo].map((item) => item.toUpperCase())
-            }
-            console.log(req.body[campo])
         }
     });
     next()
