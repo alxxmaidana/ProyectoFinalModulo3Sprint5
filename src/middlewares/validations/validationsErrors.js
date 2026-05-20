64ee039546edf3c2bd8a6d752b15c01f142c84b5
Alexander Maidana <alexanderemanuelmaidana@gmail.com>
2026-05-20 15:44:41 -0300

    Middleware que verifica errores de validación manda el array de error en el request al controlador

--- src/middlewares/validations/validationsErrors.js
+++ src/middlewares/validations/validationsErrors.js
@@ -1,15 +1,14 @@
 import { validationResult } from "express-validator";
 
+// Middleware para verificar los errores de validación
 export const verificarErroresValidacion = (req, res, next) => {
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		return res.status(400).json({
-			message: "Error de validación",
-			errors: errors.array().map((error) => ({
-				field: error.param,
-				message: error.msg,
-			})),
-		});
+	const errores = validationResult(req);
+	console.log("Errores", errores.array());
+	// Si hubo errores de validación 
+	if (!errores.isEmpty()) {
+		// Guardar los errores en req.errores para que estén disponibles en el controlador
+		req.errores = errores.array();
+		// console.log("Errores de validación encontrados:", req.errores);
 	}
 	next();
 };
